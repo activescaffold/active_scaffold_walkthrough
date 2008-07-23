d9de39d86f6ef1cd39b956f96bc3a8698873584e
Tim Harper <timcharper@gmail.com>
2008-07-22 19:18:57 -0600

    custom form column

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -2,4 +2,8 @@ module AlbumsHelper
   def price_column(record)
     number_to_currency(record.price)
   end
+  
+  def price_form_column(record, name)
+    "$%s" % text_field(:record, :price, :class => "short")
+  end
 end
