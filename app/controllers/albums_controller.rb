3b593b7ea5afb0c4363eb61a70f7baed101699d6
Tim Harper <timcharper@gmail.com>
2008-07-22 19:59:57 -0600

    changed form_ui to make artist a drop down

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,6 @@
 class AlbumsController < ApplicationController
   active_scaffold :albums do |config|
     config.columns = [:name, :album_cover, :price, :artist, :release_date, :tracks]
+    config.columns[:artist].form_ui = :select
   end
 end
