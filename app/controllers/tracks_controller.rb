3232482bfd66a103e2b0b767397bafac69dacb10
Tim Harper <timcharper@gmail.com>
2008-07-22 19:52:22 -0600

    made tracks sortable

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,8 @@
 class TracksController < ApplicationController
   active_scaffold :tracks do |config|
+    config.actions << :sortable
+    config.sortable.column = :track_number
+    
     config.columns = [:name, :sample]
   end
 end
